5fbc18f2c801105e43ab6017d9dbfa5c11e31664
dv297 <dvv297@gmail.com>
2014-10-01 21:25:00 -0400

    Attempting to switch GAE to python

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import webapp2
+
+import jinja2
+
+jinja_environment = jinja2.Environment(autoescape=True,
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        template_values = {
+            'name': 'Serendipo',
+            'verb': 'extremely happy'
+        }
+
+        template = jinja_environment.get_template('index.html')
+        self.response.out.write(template.render(template_values))
+
+app = webapp2.WSGIApplication([('/', MainPage)],
+                              debug=True)
