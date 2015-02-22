df75f404500927ed4a6f4e3f95a65ac1cce57cbb
connerda <connerda93@gmail.com>
2015-02-22 16:51:16 -0500

    other shit

--- models.py
+++ models.py
@@ -13,6 +13,7 @@ class User(webapp2_extras.appengine.auth.models.User):
     children = ndb.StringProperty(repeated=True)
     messageThreads = ndb.KeyProperty(kind ='MessageThread', repeated=True)
     classList = ndb.KeyProperty(kind='Classes', repeated=True)
+    school = ndb.KeyProperty(kind="School", repeated=True)
 
     def id(self):
         return self.key.id()
